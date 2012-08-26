076ed9f106cb7330070ae3437471194d64d81cd8
Jonjo McKay <jonjo@jonjomckay.com>
2012-08-26 23:39:26 +0100

    Fixed Dragonfly image handling and caching

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ class Norman < Sinatra::Base
 
   app = Dragonfly[:images].configure_with(:imagemagick) do |c|
     c.datastore = Dragonfly::DataStorage::MongoDataStore.new :db => Mongoid.database
-    c.url_host = 'http://0.0.0.0:9292/media'
+    c.url_host = 'http://0.0.0.0:9292'
+    c.url_format = '/media/:job'
   end
   app.define_macro_on_include(Mongoid::Document, :image_accessor)
 
@@ -24,14 +25,7 @@ class Norman < Sinatra::Base
     content_type 'application/json'
   end
 
-  get '/media/:job' do |job|
-    # TODO: Implement caching?
-    job = Dragonfly::Job.deserialize(job, app)
-    response = Dragonfly::Response.new(job, env)
-    catch(:halt) do
-      response.to_response
-    end
-  end
+  use Dragonfly::Middleware, :images
 end
 
 require_relative 'helpers'
