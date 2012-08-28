3ab1cb3542f2430a1f9dc766b836ef136c93e0f7
Jonjo McKay <jonjo@jonjomckay.com>
2012-08-28 23:52:14 +0100

    Don't want the artist name to be used as the real name by default

--- models/artist.rb
+++ models/artist.rb
@@ -24,8 +24,4 @@ class Artist
   has_many :tracks
   has_and_belongs_to_many :hashtags, inverse_of: nil
   has_and_belongs_to_many :similar_artists, class_name: "Artist", inverse_of: nil
-
-  def real_name
-    super || self[:name]
-  end
 end
