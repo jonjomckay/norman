8805238180b3177f9f6ed8ce857d5e8237896f6b
Jonjo McKay <jonjo@jonjomckay.com>
2012-09-01 23:14:28 +0100

    Renamed 'controls' to 'media'

--- routes/media.rb
+++ routes/media.rb
@@ -4,27 +4,27 @@ class Norman < Sinatra::Base
   decks = Buuren::Decks.new(:database => 'jukebox_jonjo_server_development')
 
   # GET /play
-  get '/controls/play' do
+  get '/media/play' do
     decks.play
 
-    render :rabl, :'controls/index'
+    render :rabl, :'media/index'
   end
 
   # GET /pause
-  get '/controls/pause' do
+  get '/media/pause' do
     if decks.playing?
       decks.pause
     else
       decks.play
     end
 
-    render :rabl, :'controls/index'
+    render :rabl, :'media/index'
   end
 
   # GET /next_track
-  get '/controls/next_track' do
+  get '/media/next_track' do
     decks.next_track
 
-    render :rabl, :'controls/index'
+    render :rabl, :'media/index'
   end
 end
